d65e3e4fd0633c3d1745752f0f740b5d03813662
Charlotte Evans <charlotteevans543@gmail.com>
2024-04-24 10:55:28 +0200

    Declared snake head and food Created their position and colour

--- src/snake.js
+++ src/snake.js
@@ -1,13 +1,21 @@
 
 // board
- var blockSize = 25
-var rows = 20
-var cols = 20
-var board, context;
+ const blockSize = 25 // Size of each block on the board.
+const rows = 20 // Number of rows on the board.
+const cols = 20  // Number of columns on the board.
+let board, context;
+
+// snake head
+let snakeX = blockSize * 5 // Tells the snake to start at the 5 x&y.
+let snakeY = blockSize * 5
+
+// food
+let foodX = blockSize * 10 // Tells the food to start at the 10 x&y.
+let foodY = blockSize * 10
 
 window.onload= function(){
     board = document.getElementById('board')
-    board.height = rows * blockSize
+    board.height = rows * blockSize // changes the height of the board to 500.
     board.width = cols * blockSize
     context = board.getContext('2d') // Used for drawing on the board.
 
@@ -17,4 +25,10 @@ window.onload= function(){
 function update() {
     context.fillStyle='black' // Changes the colour of the pen to black.
     context.fillRect(0,0,board.width,board.height)  // Starting from the corner of the board, filling a width and height of 500 and makes it black.
+
+    context.fillStyle='lime' // Changes the colour of the snake.
+    context.fillRect(snakeX,snakeY,blockSize,blockSize) // Draws the snake at the x and y coordinates. Snake coordinates, width and height of the canvas.
+
+    context.fillStyle='red' // Changes the colour of the food.
+    context.fillRect(foodX,foodY,blockSize,blockSize) // Draws the food at the x and y coordinates. Food coordinates, width and height of the canvas.
 }

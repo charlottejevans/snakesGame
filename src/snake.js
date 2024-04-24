6f239bab3c8f75d86688aa3d0ed41d45cd60332d
Charlotte Evans <charlotteevans543@gmail.com>
2024-04-24 11:13:12 +0200

    Declared velocity variables. Added timer so the canvas continues to update multiple times. Created logic for the snake to move.

--- src/snake.js
+++ src/snake.js
@@ -10,6 +10,8 @@ const cols = 20  // Number of columns on the board.
 let snakeX = blockSize * 5 // Tells the snake to start at the 5 x&y.
 let snakeY = blockSize * 5
 
+let velocityX = 0 // Velocity of the snake on the x axis.
+let velocityY = 0 // Velocity of the snake on the y axis.
 
 window.onload= function(){
     board = document.getElementById('board')
@@ -18,18 +20,42 @@ window.onload= function(){
     context = board.getContext('2d') // Used for drawing on the board.
 
     placeFood()
+    document.addEventListener("keyup", changeDirection)
     update()
+    setInterval(update, 1000/10) // Calls the update function every 1000/10 milliseconds because the canvas needs to be updated every 1000/10 milliseconds for the snake to move.
 }
 
 function update() {
     context.fillStyle='black' // Changes the colour of the pen to black.
     context.fillRect(0,0,board.width,board.height)  // Starting from the corner of the board, filling a width and height of 500 and makes it black.
 
-    context.fillStyle='lime' // Changes the colour of the snake.
-    context.fillRect(snakeX,snakeY,blockSize,blockSize) // Draws the snake at the x and y coordinates. Snake coordinates, width and height of the canvas.
-
     context.fillStyle='red' // Changes the colour of the food.
     context.fillRect(foodX,foodY,blockSize,blockSize) // Draws the food at the x and y coordinates. Food coordinates, width and height of the canvas.
+
+    if(snakeX === foodX && snakeY === foodY) { // If the snake eats the food, the food will be placed in a new location.
+        placeFood()
+    }
+
+    context.fillStyle='lime' // Changes the colour of the snake.
+    snakeX += velocityX * blockSize // Moves the snake on the x-axis. Moves by block size/square each time instead of 1 pixel.
+    snakeY += velocityY *blockSize // Moves the snake on the y-axis.
+    context.fillRect(snakeX,snakeY,blockSize,blockSize) // Draws the snake at the x and y coordinates. Snake coordinates, width and height of the canvas.
+ }
+
+function changeDirection(e) { // Changes the direction of the snake.
+     if (e.code === "ArrowUp" && velocityY !== 1) {
+         velocityX = 0
+         velocityY = -1
+     } else if (e.code === "ArrowDown" && velocityY !== -1) { // velocity =1 means that you cannot go the opposite direction to where you are going, otherwise you'd eat yourself.
+            velocityX = 0
+            velocityY = 1
+        } else if (e.code === "ArrowLeft" && velocityX !== 1) {
+            velocityX = -1
+            velocityY = 0
+        } else if (e.code === "ArrowRight" && velocityX !== -1) {
+            velocityX = 1
+            velocityY = 0
+     }
 }
 
 function placeFood() {
